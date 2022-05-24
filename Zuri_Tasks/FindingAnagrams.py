523a786b23f0f2c56d0b5b9c4195d27ae04a6b3e
douglas <dollarkid17@gmail.com>
2022-05-24 16:31:57 +0100

    Zuri task

--- Zuri_Tasks/FindingAnagrams.py
+++ Zuri_Tasks/FindingAnagrams.py
@@ -1,25 +1,31 @@
-str1 = "Race"
-str2 = "Care"
-
-# convert both the strings into lowercase
-str1 = str1.lower()
-str2 = str2.lower()
-result = False
+# Check if a word is an anagrams
+# Example:
+# find_anagrams("hello") --> False
+# find_anagrams("racecar") --> True
 
 
+def find_anagrams(str1, str2):
+    str1_convert = str1.lower()
+    str2_convert = str2.lower()
+    result = False
 
-# check if length is same
-if(len(str1) == len(str2)):
+    # check if length is same
+    if len(str1_convert) == len(str2_convert):
 
-    # sort the strings
-    sorted_str1 = sorted(str1)
-    sorted_str2 = sorted(str2)
+        # sort the strings
+        sorted_str1 = sorted(str1_convert)
+        sorted_str2 = sorted(str2_convert)
 
-    # if sorted char arrays are same
-    if sorted_str1 == sorted_str2:
-        result = True
+        # if sorted char arrays are same
+        if sorted_str1 == sorted_str2:
+            result = True
+        else:
+            result = False
+        return result
+
     else:
-       result = False
+        return result
 
-else:
-    result
+
+if __name__ == '__main__':
+    print(find_anagrams("Hello", "ollhe"))
